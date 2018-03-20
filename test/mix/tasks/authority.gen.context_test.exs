32994be42a778725f4438f080573fdd0e732d3ac
Ray Zane <ray@promptworks.com>
2018-03-20 17:57:42 -0400

    Fix formatting

--- test/mix/tasks/authority.gen.context_test.exs
+++ test/mix/tasks/authority.gen.context_test.exs
@@ -8,7 +8,7 @@ defmodule Authority.Ecto.Gen.ContextTest do
     within_project(fn ->
       capture_io(fn -> Generator.run(["Accounts"]) end)
 
-      assert_file "lib/authority_ecto/accounts/accounts.ex", fn file ->
+      assert_file("lib/authority_ecto/accounts/accounts.ex", fn file ->
         assert file =~ "defmodule AuthorityEcto.Accounts"
         assert file =~ "Authority.Authentication"
         assert file =~ "Authority.Tokenization"
@@ -21,90 +21,95 @@ defmodule Authority.Ecto.Gen.ContextTest do
         assert file =~ "lock_attempt_schema"
         assert file =~ "recovery_callback"
         assert file =~ "send_forgot_password_email"
-      end
+      end)
 
-      assert_file "lib/authority_ecto/accounts/user.ex", fn file ->
+      assert_file("lib/authority_ecto/accounts/user.ex", fn file ->
         assert file =~ "defmodule AuthorityEcto.Accounts.User"
-      end
+      end)
 
-      assert_file "lib/authority_ecto/accounts/token.ex", fn file ->
+      assert_file("lib/authority_ecto/accounts/token.ex", fn file ->
         assert file =~ "defmodule AuthorityEcto.Accounts.Token"
-      end
+      end)
 
-      assert_file "lib/authority_ecto/accounts/token/hmac.ex", fn file ->
+      assert_file("lib/authority_ecto/accounts/token/hmac.ex", fn file ->
         assert file =~ "defmodule AuthorityEcto.Accounts.Token.HMAC"
-      end
+      end)
 
-      assert_file "lib/authority_ecto/accounts/token/purpose.ex", fn file ->
+      assert_file("lib/authority_ecto/accounts/token/purpose.ex", fn file ->
         assert file =~ "defmodule AuthorityEcto.Accounts.Token.Purpose"
-      end
+      end)
 
-      assert_file "lib/authority_ecto/accounts/lock.ex", fn file ->
+      assert_file("lib/authority_ecto/accounts/lock.ex", fn file ->
         assert file =~ "defmodule AuthorityEcto.Accounts.Lock"
-      end
+      end)
 
-      assert_file "lib/authority_ecto/accounts/lock/reason.ex", fn file ->
+      assert_file("lib/authority_ecto/accounts/lock/reason.ex", fn file ->
         assert file =~ "defmodule AuthorityEcto.Accounts.Lock.Reason"
-      end
+      end)
 
-      assert_file "lib/authority_ecto/accounts/attempt.ex", fn file ->
+      assert_file("lib/authority_ecto/accounts/attempt.ex", fn file ->
         assert file =~ "defmodule AuthorityEcto.Accounts.Attempt"
-      end
+      end)
 
       assert [path] = Path.wildcard("priv/repo/migrations/*_authority_ecto.exs")
-      assert_file path, fn file ->
+
+      assert_file(path, fn file ->
         assert file =~ "defmodule AuthorityEcto.Repo.Migrations.AuthorityEcto"
         assert file =~ "create table(:users)"
         assert file =~ "create table(:tokens)"
         assert file =~ "references(:users, on_delete: :nothing)"
-      end
+      end)
     end)
   end
 
   test "allows skipping locking" do
     within_project(fn ->
       capture_io(fn -> Generator.run(["Accounts", "--no-locking"]) end)
-      refute_file "lib/authority_ecto/accounts/lock.ex"
-      refute_file "lib/authority_ecto/accounts/lock/reason.ex"
-      refute_file "lib/authority_ecto/accounts/attempt.ex"
-      assert_file "lib/authority_ecto/accounts/accounts.ex", fn file ->
+      refute_file("lib/authority_ecto/accounts/lock.ex")
+      refute_file("lib/authority_ecto/accounts/lock/reason.ex")
+      refute_file("lib/authority_ecto/accounts/attempt.ex")
+
+      assert_file("lib/authority_ecto/accounts/accounts.ex", fn file ->
         refute file =~ "Authority.Locking"
         refute file =~ "lock_schema"
         refute file =~ "lock_attempt_schema"
-      end
+      end)
     end)
   end
 
   test "allows skipping registration" do
     within_project(fn ->
       capture_io(fn -> Generator.run(["Accounts", "--no-registration"]) end)
-      assert_file "lib/authority_ecto/accounts/accounts.ex", fn file ->
+
+      assert_file("lib/authority_ecto/accounts/accounts.ex", fn file ->
         refute file =~ "Authority.Registration"
-      end
+      end)
     end)
   end
 
   test "allows skipping tokenization" do
     within_project(fn ->
       capture_io(fn -> Generator.run(["Accounts", "--no-tokenization"]) end)
-      refute_file "lib/authority_ecto/accounts/token.ex"
-      refute_file "lib/authority_ecto/accounts/token/hmac.ex"
-      refute_file "lib/authority_ecto/accounts/token/purpose.ex"
-      assert_file "lib/authority_ecto/accounts/accounts.ex", fn file ->
+      refute_file("lib/authority_ecto/accounts/token.ex")
+      refute_file("lib/authority_ecto/accounts/token/hmac.ex")
+      refute_file("lib/authority_ecto/accounts/token/purpose.ex")
+
+      assert_file("lib/authority_ecto/accounts/accounts.ex", fn file ->
         refute file =~ "Authority.Tokenization"
         refute file =~ "token_schema"
-      end
+      end)
     end)
   end
 
   test "allows skipping recovery" do
     within_project(fn ->
       capture_io(fn -> Generator.run(["Accounts", "--no-recovery"]) end)
-      assert_file "lib/authority_ecto/accounts/accounts.ex", fn file ->
+
+      assert_file("lib/authority_ecto/accounts/accounts.ex", fn file ->
         refute file =~ "Authority.Recovery"
         refute file =~ "recovery_callback"
         refute file =~ "send_forgot_password_email"
-      end
+      end)
     end)
   end
 
