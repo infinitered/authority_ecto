2b262044ea5d92b656fb389821c8676a8ba29394
Ray <raymondzane@gmail.com>
2018-05-11 12:57:19 -0400

    Knock out the easy tests

--- priv/templates/authority.gen.context/token/purpose_test.exs
+++ priv/templates/authority.gen.context/token/purpose_test.exs
@@ -1,3 +1,34 @@
 defmodule <%= inspect context.token_purpose.module %>Test do
-  use <%= inspect context.data_case %>
+  use ExUnit.Case, async: true
+  alias <%= inspect context.token_purpose.module %>
+
+  test "cast/1" do
+    assert Purpose.cast(:any) == {:ok, :any}
+    assert Purpose.cast("any") == {:ok, :any}
+
+    assert Purpose.cast(:recovery) == {:ok, :recovery}
+    assert Purpose.cast("recovery") == {:ok, :recovery}
+
+    assert Purpose.dump("garbage") == :error
+  end
+
+  test "dump/1" do
+    assert Purpose.dump(:any) == {:ok, "any"}
+    assert Purpose.dump("any") == {:ok, "any"}
+
+    assert Purpose.dump(:recovery) == {:ok, "recovery"}
+    assert Purpose.dump("recovery") == {:ok, "recovery"}
+
+    assert Purpose.dump("garbage") == :error
+  end
+
+  test "load/1" do
+    assert Purpose.load(:any) == :error
+    assert Purpose.load("any") == {:ok, :any}
+
+    assert Purpose.load(:recovery) == :error
+    assert Purpose.load("recovery") == {:ok, :recovery}
+
+    assert Purpose.load("garbage") == :error
+  end
 end
